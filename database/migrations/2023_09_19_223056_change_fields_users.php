7083fae60f00477064e0f4fde6617bfdea589472
Roman-Moiseenko <r.a.moiseenko@gmail.com>
2023-10-01 18:27:34 +0200

    check migrate

--- database/migrations/2023_09_19_223056_change_fields_users.php
+++ database/migrations/2023_09_19_223056_change_fields_users.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::table('users', function (Blueprint $table) {
             $table->dropColumn('name');
-            $table->string('phone')->unique();
+            $table->string('phone')->default('');
         });
     }
 
