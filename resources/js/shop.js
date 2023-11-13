b494e9686ad1bbc29baf3a33597ee9b40516feb7
Roman-Moiseenko <r.a.moiseenko@gmail.com>
2023-11-14 01:05:18 +0200

    Font-Awesome frontend

--- resources/js/shop.js
+++ resources/js/shop.js
@@ -1,4 +1,5 @@
 import './bootstrap';
+import './frontend';
 import jQuery from 'jquery';
 window.$ = jQuery;
 
--- resources/views/shop/home.blade.php
+++ resources/views/shop/home.blade.php
@@ -1,1 +1,19 @@
 @extends('layouts.shop')
+
+@section('content')
+
+<div class="container">
+    <i class="fa fa-home"></i>
+    <div class="row">
+        <div class="col-6">
+            Левая половина
+            <i class="fa-regular fa-pen-to-square"></i>
+        </div>
+        <div class="col-6">
+            Правая половина
+            <i class="fa-duotone fa-pen-to-square"></i>
+        </div>
+
+    </div>
+</div>
+@endsection
