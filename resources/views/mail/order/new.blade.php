1f1e5e1dbe2b5988a133d7d66e4b6abd05b8e132
Roman-Moiseenko <r.a.moiseenko@gmail.com>
2024-01-30 21:20:05 +0200

    Check error mail

--- resources/views/mail/order/new.blade.php
+++ resources/views/mail/order/new.blade.php
@@ -1,17 +1,18 @@
 <x-mail::message>
-# Introduction
 
-    Новый заказ {{ $order->htmlNum() }}
-    @component('mail::table')
-        | Товар       | Цена         | Цена со скидкой  | Кол-во  | Сумма  |
-        | ----------- |:------------:|:----------------:|:-------:| ------:|
-    @foreach($order->items as $item)
-        | {{ $item->product->name}}       | {{ price($item->base_cost) }}         | {{ price($item->sell_cost) }}  | {{ $item->quantity }}  | {{ price($item->sell_cost * $item->quantity) }}  |
-    @endforeach
-    @endcomponent
-    Общая сумма к оплате {{ $order->total }}
-    Ожидайте подтверждение менеджера.
-    Счет на оплату будет выслан после подтверждения
+<x-mail::panel>
+Новый заказ {{ $order->htmlNum() }}
+</x-mail::panel>
+@component('mail::table')
+| Товар       | Цена         | Цена со скидкой  | Кол-во  | Сумма  |
+| ----------- |:------------:|:----------------:|:-------:| ------:|
+@foreach($order->items as $item)
+| {{ $item->product->name}}       | {{ price($item->base_cost) }}         | {{ price($item->sell_cost) }}  | {{ $item->quantity }}  | {{ price($item->sell_cost * $item->quantity) }}  |
+@endforeach
+@endcomponent
+Общая сумма к оплате {{ $order->total }}
+Ожидайте подтверждение менеджера.
+Счет на оплату будет выслан после подтверждения
 
 Thanks,<br>
 {{ config('app.name') }}
--- resources/views/mail/user-register.blade.php
+++ resources/views/mail/user-register.blade.php
@@ -1,10 +1,10 @@
 <x-mail::message>
-    # Introduction
+# Introduction
 
-    Вы зарегистрировались на нашем сайте
+Вы зарегистрировались на нашем сайте
 
-    Вам персональный купон на {{ $coupon->bonus }} рублей.
-    Действителен до {{ $coupon->finished_at->translatedFormat('j F H:s') }}
-    Thanks,<br>
-    {{ config('app.name') }}
+Вам персональный купон на {{ $coupon->bonus }} рублей.
+Действителен до {{ $coupon->finished_at->translatedFormat('j F H:s') }}
+Thanks,<br>
+{{ config('app.name') }}
 </x-mail::message>
