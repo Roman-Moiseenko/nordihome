2d93d3a834abb446d0754975fbf5036eef3fe1be
ramoi <r.a.moiseenko@gmail.com>
2024-02-22 12:51:22 +0200

    frontend mobile Home Pgae

--- resources/views/shop/old_widget/nalich-i-zakaz2.blade.php
+++ resources/views/shop/old_widget/nalich-i-zakaz2.blade.php
@@ -1,4 +1,4 @@
-<div class="main-catalog-mini">
+<div class="main-catalog-mini nalich-i-zakaz2">
     <div class="">
         <a href="{{ route('shop.category.index') }}" class="c-mini-item">
             <img src="/images/pages/home/t-nalichie-1-min.jpg" alt="Товары Икеа в наличии">
--- resources/views/shop/old_widget/otzivi.blade.php
+++ resources/views/shop/old_widget/otzivi.blade.php
@@ -3,34 +3,40 @@
     <div class="row justify-content-center">
         <div class="col-sm-6 col-md-6 col-lg-3">
             <a href="https://yandex.ru/profile/3616123262" target="_blank" class="main-rating-item">
+                <div>
                 <i class="fa-solid fa-star"></i>
                 <i class="fa-solid fa-star"></i>
                 <i class="fa-solid fa-star"></i>
                 <i class="fa-solid fa-star"></i>
                 <i class="fa-solid fa-star"></i>
                 <span>4.7 из 5</span>
+                </div>
                 <img src="/images/pages/home/logo-yandex.svg" alt="Яндекс каталог Nordihome">
             </a>
         </div>
         <div class="col-sm-6 col-md-6 col-lg-3">
             <a href="https://2gis.ru/kaliningrad/firm/70000001062771418" target="_blank" class="main-rating-item">
+                <div>
                 <i class="fa-solid fa-star"></i>
                 <i class="fa-solid fa-star"></i>
                 <i class="fa-solid fa-star"></i>
                 <i class="fa-solid fa-star"></i>
                 <i class="fa-solid fa-star"></i>
                 <span>4.4 из 5</span>
+                </div>
                 <img src="/images/pages/home/logo-2gis.svg" alt="2гис Nordihome">
             </a>
         </div>
         <div class="col-sm-6 col-md-6 col-lg-3">
             <a href="https://maps.app.goo.gl/3b2nQBqTNWPn2MBV6" target="_blank" class="main-rating-item">
-                <i class="fa-solid fa-star"></i>
+                <div>
+                    <i class="fa-solid fa-star"></i>
                 <i class="fa-solid fa-star"></i>
                 <i class="fa-solid fa-star"></i>
                 <i class="fa-solid fa-star"></i>
                 <i class="fa-solid fa-star"></i>
                 <span>5 из 5</span>
+                </div>
                 <img src="/images/pages/home/logo-google.svg" alt="Гугл карты Nordihome">
             </a>
         </div>
@@ -38,12 +44,14 @@
             <a href="https://www.avito.ru/user/f5379553f3e3b6ac316ad9f43160852e/profile/all?sellerId=f5379553f3e3b6ac316ad9f43160852e"
                target="_blank"
                class="main-rating-item">
+                <div>
                 <i class="fa-solid fa-star"></i>
                 <i class="fa-solid fa-star"></i>
                 <i class="fa-solid fa-star"></i>
                 <i class="fa-solid fa-star"></i>
                 <i class="fa-solid fa-star"></i>
                 <span>694 отзыва</span>
+                </div>
                 <img src="/images/pages/home/avito_logo.svg" alt="Лого Авито Nordihome">
             </a>
         </div>
