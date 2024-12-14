58e59367651f5c51549bf984832bc69384aef766
Roman-Moiseenko <r.a.moiseenko@gmail.com>
2024-12-14 17:06:35 +0200

    Vue3 remove Livewire+

--- resources/views/livewire/admin/accounting/edit-comment.blade.php
+++
@@ -1,6 +0,0 @@
-<div>
-    <div class="input-group">
-        <div class="input-group-text">Комментарий</div>
-        <input wire:model="comment" wire:change="save" wire:loading.attr="disabled" class="form-control" placeholder="Комментарий" autocomplete="off">
-    </div>
-</div>
