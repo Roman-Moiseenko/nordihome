1819f009c52d872d566db232cb3cdbb5c3d58ce6
Roman-Moiseenko <r.a.moiseenko@gmail.com>
2025-01-14 18:34:13 +0200

    check++++

--- resources/js/admin.js
+++ resources/js/admin.js
@@ -1,8 +1,8 @@
 //import './dashboard';
 import "./bootstrap";
-import "./vendor/dom";
-import "./vendor/tailwind-merge";
-import "./vendor/svg-loader";
+//import "./vendor/dom";
+//import "./vendor/tailwind-merge";
+//import "./vendor/svg-loader";
 import "./functions"
 
 import jQuery from 'jquery';
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,5 @@
+import jQuery from 'jquery';
+window.$ = jQuery;
 import.meta.glob([
     '../images/**',
 ]);
