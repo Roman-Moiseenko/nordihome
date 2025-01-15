0cb728425358cc83aef159a4b9207dd488f6fc44
Roman-Moiseenko <r.a.moiseenko@gmail.com>
2025-01-16 01:31:49 +0200

    check++

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,6 +12,8 @@ class VerifyCsrfToken extends Middleware
      * @var array<int, string>
      */
     protected $except = [
+        '/api/telegram/web-hook/',
+        '/admin/mail/system/attachment',
         '/file-upload',
         '/admin/staff/photo/*',
         '/admin/accounting/bank/upload'
