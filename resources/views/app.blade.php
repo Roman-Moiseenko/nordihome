34b3b71486021cf93d9b1b61babacf7457b2501e
DESKTOP-LU20QSQ\Роман <r.a.moiseenko@gmail.com>
2025-09-21 10:45:13 +0200

    fixed

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -8,8 +8,8 @@
     <link rel="icon" href="{{ url('/images/favicon.png') }}" size="32x32">
     <!--link rel="icon" href="  Vite::asset('resources/images/admin/192x192.png')}}" size="192x192">
     <link rel="apple-touch-icon" href="  Vite::asset('resources/images/admin/180x180.png')}}" size="180x180"-->
-
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/vue/3.0.2/vue.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/vue/3.4.32/vue.cjs.min.js"></script>
+    <!--script src="https://cdnjs.cloudflare.com/ajax/libs/vue/3.0.2/vue.min.js"></script-->
     <script src="https://cdn.jsdelivr.net/npm/sortablejs@1.10.2/Sortable.min.js"></script>
     <!--script src="//cdn.jsdelivr.net/npm/@element-plus/icons-vue"></script-->
     <title inertia>{{ 'SMAP&Shop' }}</title>
