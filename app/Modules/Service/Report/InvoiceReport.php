35c6f02b98585a250741e1e8d9d15582093d9eaa
DESKTOP-LU20QSQ\Роман <r.a.moiseenko@gmail.com>
2025-09-22 12:14:36 +0200

    fixed+++++

--- app/Modules/Service/Report/InvoiceReport.php
+++ app/Modules/Service/Report/InvoiceReport.php
@@ -5,6 +5,8 @@ namespace App\Modules\Service\Report;
 
 use App\Modules\Admin\Entity\Options;
 use App\Modules\Order\Entity\Order\Order;
+use App\Modules\Order\Entity\Order\OrderItem;
+use Illuminate\Contracts\Support\Arrayable;
 use PhpOffice\PhpSpreadsheet\IOFactory;
 use PhpOffice\PhpSpreadsheet\Spreadsheet;
 use PhpOffice\PhpSpreadsheet\Writer\Pdf\Mpdf;
@@ -14,6 +16,8 @@ class InvoiceReport
 {
     private string $template;
     private ReportService $service;
+
+    private const int BEGIN_ROW = 15;
 
     public function __construct(ReportService $service)
     {
@@ -61,7 +65,7 @@ class InvoiceReport
             '{corr_account}' => $order->trader->corr_account,
             '{pay_account}' => $order->trader->pay_account,
             '{chief}' => $order->trader->chief->getShortname(),
-            '{vat_caption}' => $vat_order == 0 ? 'Без налога (НДС)' : 'В том числе НДС',
+            '{vat_caption}' => $vat_order == 0 ? 'Без налога (НДС)' : 'В том числе НДС ' . '(' . $order->getVATTrader() . '%)',
             '{vat_value}' => $vat_order == 0 ? '-' : $vat_order,
         ];
 
@@ -69,30 +73,39 @@ class InvoiceReport
 
         //Список товара
         $begin_row_products = 15; //TODO Перенести в настройки отчетов
-        $_from = $begin_row_products;
-        $_to = $begin_row_products + 1;
+        $_from = self::BEGIN_ROW;
+        $_to = self::BEGIN_ROW + 1;
         $count_items = $order->items()->count();
         $count_additions = $order->additions()->count();
 
-        foreach ($order->items as $i => $item) {
+        $this->OrderItemsToSheet($order, $activeWorksheet, $_from, $_to, false);
+        $this->OrderItemsToSheet($order, $activeWorksheet, $_from, $_to, true);
+
+      /*
+        foreach ($order->items()->where('preorder', false)->get() as $i => $item) {
 
             if ($count_additions != 0 || $i != $count_items - 1) {
                 $activeWorksheet->insertNewRowBefore($_from, 1);
                 $this->service->copyRowsRange($activeWorksheet, 'A' . $_to . ':J' . $_to, 'A' . $_from);
             }
 
-            $activeWorksheet->setCellValue([1, $begin_row_products + $i], ($i + 1));
-            $activeWorksheet->setCellValue([2, $begin_row_products + $i], $item->product->name);
-            $activeWorksheet->setCellValue([6, $begin_row_products + $i], $item->product->code);
-            $activeWorksheet->setCellValue([7, $begin_row_products + $i], $item->quantity);
-            $activeWorksheet->setCellValue([8, $begin_row_products + $i], $item->product->measuring->name);
-            $activeWorksheet->setCellValue([9, $begin_row_products + $i], price($item->sell_cost));
-            $activeWorksheet->setCellValue([10, $begin_row_products + $i], price($item->sell_cost * $item->quantity));
-
+            $this->ItemToSheet($activeWorksheet, $item, $begin_row_products, $i);
             $_from++;
             $_to++;
         }
 
+        foreach ($order->items()->where('preorder', true)->get() as $i => $item) {
+
+            if ($count_additions != 0 || $i != $count_items - 1) {
+                $activeWorksheet->insertNewRowBefore($_from, 1);
+                $this->service->copyRowsRange($activeWorksheet, 'A' . $_to . ':J' . $_to, 'A' . $_from);
+            }
+
+            $this->ItemToSheet($activeWorksheet, $item, $i);
+            $_from++;
+            $_to++;
+        }
+*/
         //Список Услуг
         foreach ($order->additions as $j => $addition) {
 
@@ -101,13 +114,13 @@ class InvoiceReport
                 $this->service->copyRowsRange($activeWorksheet, 'A' . $_to . ':J' . $_to, 'A' . $_from);
             }
 
-            $activeWorksheet->setCellValue([1, $begin_row_products + $count_items + $j], ($j + 1 + $count_items));
-            $activeWorksheet->setCellValue([2, $begin_row_products + $count_items + $j], $addition->addition->name);
-            $activeWorksheet->setCellValue([6, $begin_row_products + $count_items + $j], '-');
-            $activeWorksheet->setCellValue([7, $begin_row_products + $count_items + $j], 1);
-            $activeWorksheet->setCellValue([8, $begin_row_products + $count_items + $j], 'услуга');
-            $activeWorksheet->setCellValue([9, $begin_row_products + $count_items + $j], price($addition->amount));
-            $activeWorksheet->setCellValue([10, $begin_row_products + $count_items + $j], price($addition->amount));
+            $activeWorksheet->setCellValue([1, self::BEGIN_ROW + $count_items + $j], ($j + 1 + $count_items));
+            $activeWorksheet->setCellValue([2, self::BEGIN_ROW + $count_items + $j], $addition->addition->name);
+            $activeWorksheet->setCellValue([6, self::BEGIN_ROW + $count_items + $j], '-');
+            $activeWorksheet->setCellValue([7, self::BEGIN_ROW + $count_items + $j], 1);
+            $activeWorksheet->setCellValue([8, self::BEGIN_ROW + $count_items + $j], 'услуга');
+            $activeWorksheet->setCellValue([9, self::BEGIN_ROW + $count_items + $j], price($addition->amount));
+            $activeWorksheet->setCellValue([10, self::BEGIN_ROW + $count_items + $j], price($addition->amount));
 
             $_from++;
             $_to++;
@@ -117,6 +130,45 @@ class InvoiceReport
         $spreadsheet->getActiveSheet()->getPageSetup()->setFitToHeight(1);
 
         return $spreadsheet;
+    }
+
+    private function OrderItemsToSheet(Order $order, $activeWorksheet,
+                                       int &$from, int &$to,
+                                       bool|null $preorder = null): void
+    {
+        $count_items = $order->items()->count();
+        $count_additions = $order->additions()->count();
+
+        if (is_null($preorder)) {
+            $items = $order->items()->get();
+        } else if($preorder) {
+            $items = $order->items()->where('preorder', false)->get();
+        } else {
+            $items = $order->items()->where('preorder', true)->get();
+        }
+        foreach ($items as $i => $item) {
+
+            if ($count_additions != 0 || $i != $count_items - 1) {
+                $activeWorksheet->insertNewRowBefore($from, 1);
+                $this->service->copyRowsRange($activeWorksheet, 'A' . $to . ':J' . $to, 'A' . $from);
+            }
+
+            $this->ItemToSheet($activeWorksheet, $item, $i);
+            $from++;
+            $to++;
+        }
+
+    }
+
+    private function ItemToSheet($activeWorksheet, OrderItem $item, $i): void
+    {
+        $activeWorksheet->setCellValue([1, self::BEGIN_ROW + $i], ($i + 1));
+        $activeWorksheet->setCellValue([2, self::BEGIN_ROW + $i], $item->product->name);
+        $activeWorksheet->setCellValue([6, self::BEGIN_ROW + $i], $item->product->code);
+        $activeWorksheet->setCellValue([7, self::BEGIN_ROW + $i], $item->quantity);
+        $activeWorksheet->setCellValue([8, self::BEGIN_ROW + $i], $item->product->measuring->name);
+        $activeWorksheet->setCellValue([9, self::BEGIN_ROW + $i], price($item->sell_cost));
+        $activeWorksheet->setCellValue([10, self::BEGIN_ROW + $i], price($item->sell_cost * $item->quantity));
     }
 
     public function xlsx(Order $order): string
