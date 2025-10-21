1b4cb42b6ca52c4c1dd2edcb185cf29ab6d5b36d
PC2MSIMAG <w@mail.ru>
2025-10-22 00:02:08 +0200

    test2

--- resources/views/shop/nordihome/templates/text/text04.blade.php
+++ resources/views/shop/nordihome/templates/text/text04.blade.php
@@ -16,7 +16,7 @@
     */
     /** @var \App\Modules\Page\Entity\TextWidget $widget */
 @endphp
-<div>
+<div class="p-t_50 p-b_50 bg-blue f-z_23 t-a_center t-t_uppercase f-w_600">
     @foreach($widget->items as $item)
         <div class="text-center m-b_30 notification-top">
             {!! $item->text !!}
