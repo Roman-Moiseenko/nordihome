507122a304aecb3ce748159492521ff892427f4d
PC2MSIMAG <w@mail.ru>
2025-10-21 23:58:22 +0200

    test2

---
+++ resources/views/shop/nordihome/templates/text/text04.blade.php
@@ -0,0 +1,25 @@
+<!--template:Оповещение на главной вверху-->
+@php
+    /**
+    * TextWidget::class - string
+    * $widget->caption - string
+    * $widget->description - string
+    * $widget->image - Photo::class
+    * $widget->icon - Photo::class
+    * TextWidgetItem:class
+    * $widget->items - Arraible
+    * $widget->itemBySlug(string)?: TextWidgetItem
+    * $item->caption -
+    * $item->description -
+    * $item->text - text (форматируемый текст)
+
+    */
+    /** @var \App\Modules\Page\Entity\TextWidget $widget */
+@endphp
+<div>
+    @foreach($widget->items as $item)
+        <div class="text-center m-b_30 notification-top">
+            {!! $item->text !!}
+        </div>
+    @endforeach
+</div>
