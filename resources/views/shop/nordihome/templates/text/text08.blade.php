0e9d60921652c542fc3f3264e158eae581ea1c69
PC2MSIMAG <w@mail.ru>
2025-10-23 21:01:23 +0200

    test2

--- resources/views/shop/nordihome/templates/text/text08.blade.php
+++ resources/views/shop/nordihome/templates/text/text08.blade.php
@@ -29,7 +29,7 @@
         @endforeach
     </div>
 </div>
-<div class="accordion accordion_1" id="accordion-faq">
+<div class="accordion accordion_faq" id="faq-tab">
     @foreach($widget->items as $item)
         <div class="accordion-item">
             <div class="accordion-header" id="panelsStayOpen-heading{{$item->slug}}">
