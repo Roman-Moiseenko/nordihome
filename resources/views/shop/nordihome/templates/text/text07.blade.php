3852c7f11f39e3f1ba17f63387c0dfa0321b61db
PC2MSIMAG <w@mail.ru>
2025-10-29 02:12:07 +0200

    test2

--- resources/views/shop/nordihome/templates/text/text07.blade.php
+++ resources/views/shop/nordihome/templates/text/text07.blade.php
@@ -17,59 +17,61 @@
     /** @var \App\Modules\Page\Entity\TextWidget $widget */
 @endphp
 <div class="main-reviews p-t_50 p-b_50" id="reviews-tab">
-    <h2 class="t-t_uppercase t-a_center">{{ $widget->caption }}</h2>
-    <div class="row justify-content-center">
-        <div class="col-sm-6 col-md-6 col-lg-4">
-            <a href="https://yandex.ru/profile/3616123262" target="_blank" class="main-rating-item">
-                <ul class="star">
-                    <li class="one"></li>
-                    <li class="one"></li>
-                    <li class="one"></li>
-                    <li class="one"></li>
-                    <li class="one"></li>
-                    <li>4.7 из 5</li>
-                </ul>
-                <img src="https://nordihome.ru/wp-content/themes/euroikea/images/logo-yandex.svg" alt="Яндекс каталог Nordihome">
-            </a>
-        </div>
-        <!-- <div class="col-sm-6 col-md-6 col-lg-3">
-            <div class="main-rating-item">
-                <ul class="star">
-                    <li class="one"></li>
-                    <li class="one"></li>
-                    <li class="one"></li>
-                    <li class="one"></li>
-                    <li class="one"></li>
-                    <li>4.4 из 5</li>
-                </ul>
-                <img src="/wp-content/themes/euroikea/images/logo-2gis.svg" alt="2гис Nordihome">
+    <div class="container">
+        <h2 class="t-t_uppercase t-a_center">{{ $widget->caption }}</h2>
+        <div class="row justify-content-center">
+            <div class="col-sm-6 col-md-6 col-lg-4">
+                <a href="https://yandex.ru/profile/3616123262" target="_blank" class="main-rating-item">
+                    <ul class="star">
+                        <li class="one"></li>
+                        <li class="one"></li>
+                        <li class="one"></li>
+                        <li class="one"></li>
+                        <li class="one"></li>
+                        <li>4.7 из 5</li>
+                    </ul>
+                    <img src="https://nordihome.ru/wp-content/themes/euroikea/images/logo-yandex.svg" alt="Яндекс каталог Nordihome">
+                </a>
             </div>
-        </div> -->
-        <!-- <div class="col-sm-6 col-md-6 col-lg-3">
-            <div class="main-rating-item">
-                <ul class="star">
-                    <li class="one"></li>
-                    <li class="one"></li>
-                    <li class="one"></li>
-                    <li class="one"></li>
-                    <li class="one"></li>
-                    <li>5 из 5</li>
-                </ul>
-                <img src="/wp-content/themes/euroikea/images/logo-google.svg" alt="Гугл карты Nordihome">
+            <!-- <div class="col-sm-6 col-md-6 col-lg-3">
+                <div class="main-rating-item">
+                    <ul class="star">
+                        <li class="one"></li>
+                        <li class="one"></li>
+                        <li class="one"></li>
+                        <li class="one"></li>
+                        <li class="one"></li>
+                        <li>4.4 из 5</li>
+                    </ul>
+                    <img src="/wp-content/themes/euroikea/images/logo-2gis.svg" alt="2гис Nordihome">
+                </div>
+            </div> -->
+            <!-- <div class="col-sm-6 col-md-6 col-lg-3">
+                <div class="main-rating-item">
+                    <ul class="star">
+                        <li class="one"></li>
+                        <li class="one"></li>
+                        <li class="one"></li>
+                        <li class="one"></li>
+                        <li class="one"></li>
+                        <li>5 из 5</li>
+                    </ul>
+                    <img src="/wp-content/themes/euroikea/images/logo-google.svg" alt="Гугл карты Nordihome">
+                </div>
+            </div> -->
+            <div class="col-sm-6 col-md-6 col-lg-4">
+                <a href="https://www.avito.ru/brands/nordihome.ru/all?sellerId=77c3371d8da225e083f29aea5b416174" target="_blank" class="main-rating-item">
+                    <ul class="star">
+                        <li class="one"></li>
+                        <li class="one"></li>
+                        <li class="one"></li>
+                        <li class="one"></li>
+                        <li class="one"></li>
+                        <li>1025 отзывов</li>
+                    </ul>
+                    <img src="https://nordihome.ru/wp-content/themes/euroikea/images/avito_logo.svg" alt="Лого Авито Nordihome">
+                </a>
             </div>
-        </div> -->
-        <div class="col-sm-6 col-md-6 col-lg-4">
-            <a href="https://www.avito.ru/brands/nordihome.ru/all?sellerId=77c3371d8da225e083f29aea5b416174" target="_blank" class="main-rating-item">
-                <ul class="star">
-                    <li class="one"></li>
-                    <li class="one"></li>
-                    <li class="one"></li>
-                    <li class="one"></li>
-                    <li class="one"></li>
-                    <li>1025 отзывов</li>
-                </ul>
-                <img src="https://nordihome.ru/wp-content/themes/euroikea/images/avito_logo.svg" alt="Лого Авито Nordihome">
-            </a>
         </div>
     </div>
 </div>
