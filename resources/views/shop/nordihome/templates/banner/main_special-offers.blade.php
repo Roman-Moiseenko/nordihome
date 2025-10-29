4a90edb2605e27d911ceb65838fe237ae3a5741b
PC2MSIMAG <w@mail.ru>
2025-10-29 23:09:41 +0200

    test2

--- resources/views/shop/nordihome/templates/banner/main_special-offers.blade.php
+++ resources/views/shop/nordihome/templates/banner/main_special-offers.blade.php
@@ -15,7 +15,7 @@
 @endphp
 <div class="main-slider-sale p-t_50 p-b_50">
     <div class="container">
-        <h2 class="t-t_uppercase t-a_center">Почему нас выбирают</h2>
+        <h2 class="t-t_uppercase t-a_center">{{ $banner->caption }}</h2>
         <div id="slider-payment" class="owl-carousel owl-theme">
             @foreach($widget->items as $item)
                 <div>
