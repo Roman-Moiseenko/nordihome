ccd7f7ed15a6e08f3691ba9c2c40cc491a8022d5
PC2MSIMAG <w@mail.ru>
2025-10-30 01:23:14 +0200

    test2

--- resources/views/shop/nordihome/templates/banner/main_reviews.blade.php
+++ resources/views/shop/nordihome/templates/banner/main_reviews.blade.php
@@ -1,1 +1,29 @@
-<?php
+<!--template:Главная - слайдер отзывы клиентов-->
+@php
+    /**
+    * Banner::class - string
+    * $banner->caption - string
+    * $banner->description - string
+    * BannerItem:class
+    * $banner->items - Arraible
+    * $item->image - Photo::class
+    * $item->url - string
+    * $item->caption - string
+    * $item->description - string
+    */
+    /** @var \App\Modules\Page\Entity\BannerWidget $widget */
+@endphp
+<div class="main-reviews p-t_50 p-b_50" id="reviews-tab">
+    <div class="container-fluid">
+        <h2 class="t-t_uppercase t-a_center">{{ $widget->caption }}</h2>
+        <div id="slider-payment" class="owl-carousel owl-theme">
+            @foreach($widget->items as $item)
+                <div>
+                    <a href="{{ $item->url }}">
+                        <img src="{{ $item->getImage() }}"/>
+                    </a>
+                </div>
+            @endforeach
+        </div>
+    </div>
+</div>
