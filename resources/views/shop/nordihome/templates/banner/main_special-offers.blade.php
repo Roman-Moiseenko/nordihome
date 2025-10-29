eb2e413fd5ffc1c56b7d114cc219d90f21fc894f
PC2MSIMAG <w@mail.ru>
2025-10-29 23:45:35 +0200

    test2

--- resources/views/shop/nordihome/templates/banner/main_special-offers.blade.php
+++ resources/views/shop/nordihome/templates/banner/main_special-offers.blade.php
@@ -1,4 +1,4 @@
-<!--template:Главная - слайдер специальные предложения-->
+<!--template:Главная слайдер специальные предложения-->
 @php
     /**
     * Banner::class - string
@@ -15,16 +15,14 @@
 @endphp
 <div class="main-slider-sale p-t_50 p-b_50">
     <div class="container">
-        <h2 class="t-t_uppercase t-a_center">{{ $banner->caption }}</h2>
-        <div id="slider-payment" class="owl-carousel owl-theme main-slider">
-
-                @foreach($widget->items as $item)
-                    <div>
-                        <a href="{{ $item->url }}">
-                            <img src="{{ $item->getImage() }}"/>
-                        </a>
-                    </div>
-                @endforeach
+        <div id="slider-payment" class="owl-carousel owl-theme">
+            @foreach($widget->items as $item)
+                <div>
+                    <a href="{{ $item->url }}">
+                        <img src="{{ $item->getImage() }}"/>
+                    </a>
+                </div>
+            @endforeach
         </div>
     </div>
 </div>
