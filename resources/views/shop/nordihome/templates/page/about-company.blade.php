2a36bff661146208d751f3107db06471f8918e87
PC2MSIMAG <w@mail.ru>
2025-11-08 00:23:47 +0200

    test2

--- resources/views/shop/nordihome/templates/page/about-company.blade.php
+++ resources/views/shop/nordihome/templates/page/about-company.blade.php
@@ -9,7 +9,7 @@
 @section('description', $description)
 
 @section('content')
-    <h1 class="my-4">{{ $page->name }}</h1>
+    <div class="container-xl"><h1 class="my-4">{{ $page->name }}</h1></div>
     <div class="mt-4">
         {!! $page->text !!}
     </div>
