b3ae18fd87c442fe3249a1b0a1dc24f60c3e786a
PC2MSIMAG <w@mail.ru>
2025-12-07 18:48:23 +0200

    test2

--- resources/views/shop/nordihome/templates/page/contact.blade.php
+++ resources/views/shop/nordihome/templates/page/contact.blade.php
@@ -1,3 +1,4 @@
+<!--template:Обычная текстовая страница-->
 @extends('shop.nordihome.layouts.main')
 
 @section('main')
@@ -8,34 +9,8 @@
 @section('description', $description)
 
 @section('content')
-
-    <div class="container-xl">
-        <h1>{{ $page->name }}</h1>
-        <div class="row mt-4">
-            <div class="col-lg-6 ps-md-4" style="display: grid">
-                <div class="about-block">
-                    <div class="heading-border">
-                        О КОМПАНИИ
-                    </div>
-                    <p>
-                        NORDI HOME — Бренд, успешно работающий в Калининграде с 2020 года. Более 7 000 счастливых клиентов!
-                        Компания занимается продажей и доставкой мебели ИКЕА из Европы под ключ для вашего удобства. Ниже
-                        можно ознакомиться более подробно с условиями и тарифами на доставку
-                    </p>
-                    <p>
-                        Также в <a href="{{ route('shop.category.index') }}">нашем каталоге</a> Вы можете выбрать и купить мебель ИКЕА уже сегодня!
-                    </p>
-                    <div class="mt-3">
-                        <a href="{{ route('shop.category.index') }}" class="btn btn-light rounded-pill">ПЕРЕЙТИ В КАТАЛОГ</a>
-                    </div>
-                </div>
-
-            </div>
-            <div class="col-lg-6 pe-md-4">
-                <a href="{{ route('shop.category.index') }}" >
-                    <img src="/images/pages/about.jpg" style="width: 100%" alt="Каталог интернет магазина NORDI HOME">
-                </a>
-            </div>
+        <h1 class="my-4">{{ $page->name }}</h1>
+        <div class="mt-4">
+            {!! $page->text !!}
         </div>
-    </div>
 @endsection
