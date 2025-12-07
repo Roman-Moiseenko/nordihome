6b7e3941eb8715c7201d4791e18206f4b66ee4d5
PC2MSIMAG <w@mail.ru>
2025-12-07 18:43:23 +0200

    test2

--- resources/views/shop/nordihome/templates/page/text.blade.php
+++ resources/views/shop/nordihome/templates/page/text.blade.php
@@ -9,10 +9,8 @@
 @section('description', $description)
 
 @section('content')
-    <div class="container-xl">
         <h1 class="my-4">{{ $page->name }}</h1>
         <div class="mt-4">
             {!! $page->text !!}
         </div>
-    </div>
 @endsection
