5f8efa86287ef5f877b50540c0c51c5944b690f9
PC2MSIMAG <w@mail.ru>
2025-12-09 00:58:15 +0200

    test2

--- resources/views/shop/nordihome/templates/page/ikea-kitchens.blade.php
+++ resources/views/shop/nordihome/templates/page/ikea-kitchens.blade.php
@@ -96,6 +96,33 @@
         <div class="t-a_center t-color_orange t-t_uppercase m-b_10 f-z_28 f-w_600">Одна система - тысячи возможностей!</div>
         <div class="t-a_center m-b_30">Вы можете собрать свой вариант кухни из отдельных элементов, или выбрать готовую комплектацию в нашем каталоге. ИКЕА предлагает несколько линеек готовых модульных кухонь, которые различаются по дизайну, функциональности и стоимости.</div>
         <div class="m-b_20">тут слайдер с товарами</div>
-        <div class="t-a_center"><a href="/kuhni-pod-zakaz/" class="btn btn-black">СМОТРЕТЬ ВСЕ КУХНИ</a></div>
+        <div class="t-a_center m-b_20"><a href="/" class="btn btn-black">СМОТРЕТЬ ВСЕ КУХНИ</a></div>
+        <div class="quality-kitchens">
+            <h2 class="page-h2"><span class="t-color_orange">Преимущества кухонь</span> от ИКЕА:</h2>
+            <div class="row">
+                <div class="col-lg-6">
+                    <div class="quality-kitchens-text">
+                        <h3 class="t-color_orange">1. КАЧЕСТВО</h3>
+                        <p>Главный критерий, который уже много лет заставляет людей вновь и вновь выбирать ИКЕА.</p>
+                        <h3 class="t-color_orange">2. ФУНКЦИОНАЛЬНОСТЬ</h3>
+                        <p class="m-b_50">Умные модульные решения помогут обустроить каждый сантиметр кухни. Это делает кухни ИКЕА отличной альтернативой мебели на заказ.</p>
+                        <img src="/uploads/gallery/1/kitchen01-min.jpg" alt="Кухни под заказ" class="m-b_10">
+                        <img src="/uploads/gallery/1/kitchen03-min.jpg" alt="Кухни под заказ">
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <div class="quality-kitchens-text">
+                        <img src="/uploads/gallery/1/kitchen02-min.jpg" alt="Кухни под заказ">
+                        <h3 class="t-color_orange">3. МОБИЛЬНОСТЬ И ЛЁГКОСТЬ СБОРКИ</h3>
+                        <p>Кухни от ИКЕА разработаны по модульному принципу, сборка напоминает конструктор: соединяй что хочешь и как хочешь.</p>
+                        <h3 class="t-color_orange">4. СТИЛЬНЫЕ РЕШЕНИЯ</h3>
+                        <p>Дизайнеры по всему миру используют кухни ИКЕА в своих проектах. Широкая палитра цветов и стилей позволяет комбинировать каркасы, фасады и внутреннее наполнение.</p>
+                        <h3 class="t-color_orange">5. ДОСТУПНАЯ ЦЕНА</h3>
+                        <p class="m-b_30">Отличное качество по разумным ценам.</p>
+                        <img src="/uploads/gallery/1/kitchen04-min.jpg" alt="Кухни под заказ">
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 @endsection
