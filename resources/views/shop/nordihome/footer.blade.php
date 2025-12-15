03127c2b48f13b2573a702d2e758faab192656c4
PC2MSIMAG <w@mail.ru>
2025-12-16 00:40:58 +0200

    test2

--- resources/views/shop/nordihome/footer.blade.php
+++ resources/views/shop/nordihome/footer.blade.php
@@ -38,9 +38,9 @@
             </div>
             <div class="col-12 col-sm-6 col-md-6 col-lg-3">
                 <div class="heading f-w_600 m-b_20">ДЛЯ КЛИЕНТА</div>
-                @if(isset($menus['menu-footer02']))
+                @if(isset($menus['menu-footer07']))
                 <ul class="footer-menu">
-                    @foreach($menus['menu-footer07']['items'] as $item)
+                    @foreach($menus['menu-footer02']['items'] as $item)
                         <li><a href="{{ $item['url'] }}">{{ $item['name'] }}</a></li>
                     @endforeach
                 </ul>
