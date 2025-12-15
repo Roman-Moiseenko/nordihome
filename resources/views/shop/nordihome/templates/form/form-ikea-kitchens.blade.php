c08ef50458ba93125212fca824311cce32ef7422
PC2MSIMAG <w@mail.ru>
2025-12-16 01:18:15 +0200

    test2

--- resources/views/shop/nordihome/templates/form/form-ikea-kitchens.blade.php
+++ resources/views/shop/nordihome/templates/form/form-ikea-kitchens.blade.php
@@ -38,7 +38,9 @@ use App\Modules\Page\Entity\FormWidget;
                         </label>
                     </div>
                     <div class="col-md-6 col-lg-6 m-b_5">
-
+                        <label>
+                            <input name="city" required placeholder="{{ $widget->fields["city"] }}"/>
+                        </label>
                     </div>
                     <div class="col-md-6 col-lg-6 m-b_5"></div>
                     <div class="col-12"></div>
