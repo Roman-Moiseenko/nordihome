2dd96986e49252891194bb84e50f91b1a0dce63a
PC2MSIMAG <w@mail.ru>
2025-12-21 01:54:28 +0200

    test2

--- resources/views/shop/nordihome/templates/post/article-modular kitchens.blade.php
+++ resources/views/shop/nordihome/templates/post/article-modular kitchens.blade.php
@@ -1,1 +1,22 @@
-<?php
+<!--template:Запись шаблон по-умолчанию-->
+@php
+    /** @var \App\Modules\Page\Entity\Post $post */
+    $photo = photo_std('main_001', 'card');
+@endphp
+@extends('shop.nordihome.layouts.main')
+
+@section('main')
+    post
+@endsection
+
+@section('title', $title)
+@section('description', $description)
+
+@section('content')
+    <div class="container-xl">
+        <h1 class="my-4">{{ $post->title }}</h1>
+    </div>
+    <div>
+        {!! $post->text !!}
+    </div>
+@endsection
