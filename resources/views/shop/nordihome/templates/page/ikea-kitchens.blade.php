393ddbf8c8e346c1d83d4ce913759042bbd7f4b0
PC2MSIMAG <w@mail.ru>
2025-12-20 03:08:48 +0200

    test2

--- resources/views/shop/nordihome/templates/page/ikea-kitchens.blade.php
+++ resources/views/shop/nordihome/templates/page/ikea-kitchens.blade.php
@@ -12,9 +12,8 @@
     <div class="container-xl">
         <h1 class="my-4">{{ $page->name }}</h1>
     </div>
-    <div class="container-xl">
         <div class="mt-4">
             {!! $page->text !!}
         </div>
-    </div>
+
 @endsection
