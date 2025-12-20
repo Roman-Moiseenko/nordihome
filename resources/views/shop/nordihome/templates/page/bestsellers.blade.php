ffc308ce755d263589b3cb154efc4a7dc07984c2
PC2MSIMAG <w@mail.ru>
2025-12-20 17:55:20 +0200

    test2

--- resources/views/shop/nordihome/templates/page/bestsellers.blade.php
+++ resources/views/shop/nordihome/templates/page/bestsellers.blade.php
@@ -1,1 +1,19 @@
 <!--template:Страница Хиты продаж -->
+@extends('shop.nordihome.layouts.main')
+
+@section('main')
+    pages
+@endsection
+
+@section('title', $title)
+@section('description', $description)
+
+@section('content')
+    <div class="container-xl">
+        <h1 class="my-4">{{ $page->name }}</h1>
+    </div>
+    <div class="mt-4">
+        {!! $page->text !!}
+    </div>
+
+@endsection
