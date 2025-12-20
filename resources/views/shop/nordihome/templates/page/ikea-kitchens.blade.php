0fabaa1c3aee5517aa54145ecd4bbaec440cdc46
PC2MSIMAG <w@mail.ru>
2025-12-20 02:55:26 +0200

    test2

--- resources/views/shop/nordihome/templates/page/ikea-kitchens.blade.php
+++ resources/views/shop/nordihome/templates/page/ikea-kitchens.blade.php
@@ -16,33 +16,5 @@
         <div class="mt-4">
             {!! $page->text !!}
         </div>
-        <div class="t-a_center m-b_20"><a href="/" class="btn btn-black">СМОТРЕТЬ ВСЕ КУХНИ</a></div>
-        <div class="quality-kitchens">
-            <h2 class="page-h2"><span class="t-color_orange">Преимущества кухонь</span> от ИКЕА:</h2>
-            <div class="row">
-                <div class="col-lg-6">
-                    <div class="quality-kitchens-text">
-                        <h3 class="t-color_orange">1. КАЧЕСТВО</h3>
-                        <p>Главный критерий, который уже много лет заставляет людей вновь и вновь выбирать ИКЕА.</p>
-                        <h3 class="t-color_orange">2. ФУНКЦИОНАЛЬНОСТЬ</h3>
-                        <p class="m-b_50">Умные модульные решения помогут обустроить каждый сантиметр кухни. Это делает кухни ИКЕА отличной альтернативой мебели на заказ.</p>
-                        <img src="/uploads/gallery/1/kitchen01-min.jpg" class="width_100 m-b_10" alt="Кухни под заказ">
-                        <img src="/uploads/gallery/1/kitchen03-min.jpg" class="width_100" alt="Кухни под заказ">
-                    </div>
-                </div>
-                <div class="col-lg-6">
-                    <div class="quality-kitchens-text">
-                        <img src="/uploads/gallery/1/kitchen02-min.jpg" class="width_100" alt="Кухни под заказ">
-                        <h3 class="t-color_orange">3. МОБИЛЬНОСТЬ И ЛЁГКОСТЬ СБОРКИ</h3>
-                        <p>Кухни от ИКЕА разработаны по модульному принципу, сборка напоминает конструктор: соединяй что хочешь и как хочешь.</p>
-                        <h3 class="t-color_orange">4. СТИЛЬНЫЕ РЕШЕНИЯ</h3>
-                        <p>Дизайнеры по всему миру используют кухни ИКЕА в своих проектах. Широкая палитра цветов и стилей позволяет комбинировать каркасы, фасады и внутреннее наполнение.</p>
-                        <h3 class="t-color_orange">5. ДОСТУПНАЯ ЦЕНА</h3>
-                        <p class="m-b_30">Отличное качество по разумным ценам.</p>
-                        <img src="/uploads/gallery/1/kitchen04-min.jpg" class="width_100" alt="Кухни под заказ">
-                    </div>
-                </div>
-            </div>
-        </div>
     </div>
 @endsection
