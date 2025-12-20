653f5f5f43063e7f092c024e650b767e044bbb01
PC2MSIMAG <w@mail.ru>
2025-12-20 17:57:43 +0200

    test2

--- resources/views/shop/nordihome/templates/page/bestsellers.blade.php
+++ resources/views/shop/nordihome/templates/page/bestsellers.blade.php
@@ -11,9 +11,10 @@
 @section('content')
     <div class="container-xl">
         <h1 class="my-4">{{ $page->name }}</h1>
+        <div class="mt-4">
+            {!! $page->text !!}
+        </div>
     </div>
-    <div class="mt-4">
-        {!! $page->text !!}
-    </div>
+
 
 @endsection
