126169d180b4cad802704b2686312644232305bb
PC2MSIMAG <w@mail.ru>
2025-12-22 00:27:23 +0200

    test2

--- resources/views/shop/nordihome/templates/form/form-article-modular-kitchens.blade.php
+++ resources/views/shop/nordihome/templates/form/form-article-modular-kitchens.blade.php
@@ -19,46 +19,41 @@
         use App\Modules\Page\Entity\FormWidget;
         /** @var FormWidget $widget  */
         @endphp
-        <div class="main-kitchen">
-            <img src="/uploads/gallery/1/main-kuhni-min.jpg" class="width_100" alt="Кухни икеа под заказ">
-            <div class="main-kitchen-form bg-white">
-                <div class="t-t_uppercase f-w_600 t-a_center f-z_23 m-b_10">Получите <span class="t-color_orange">дизайн-проект в подарок!*</span></div>
-                <div class="t-a_center m-b_10">Наш дизайнер с опытом работы более 10 лет в ИКЕА разработает дизайн-проект вашей кухни!</div>
-                <div class="m-b_10">
-                    <div id="{{ $widget->id }}" class="feedback" not-hide>
-                        <div class="row">
-                            <div class="col-md-6 col-lg-2">
-                                <label>
-                                    <input name="name" type="text" required placeholder="{{ $widget->fields["name"] }}"/>
-                                </label>
-                            </div>
-                            <div class="col-md-6 col-lg-3">
-                                <label>
-                                    <input name="phone" type="tel" required placeholder="{{ $widget->fields["phone"] }}"/>
-                                </label>
-                            </div>
-                            <div class="col-md-9 col-lg-5">
-                                <label>
-                                    <textarea placeholder="{{ $widget->fields["question"] }}"></textarea>
-                                </label>
-                            </div>
-                            <div class="col-md-3 col-lg-2">
-                                <label><button class="btn-form" type="button">ОТПРАВИТЬ</button></label>
-                            </div>
-                            <div class="col-12">
-                                <label class="f-z_14">
-                                    <input type="checkbox" name="agreement" value="{{ $widget->fields["agreement"] }}"> Я <a href="/page/soglasie-na-obrabotku-personalnyx-dannyx" target="_blank">согласен</a> на обработку персональных данных. Подробнее об этом в <a href="/page/politika-obrabotki-personalnyx-dannyx" target="_blank">политике конфиденциальности</a>
-                                </label>
-                            </div>
-                        </div>
-                    </div>
-                    <div id="{{ $widget->id }}-callback" class="form-send-message" style="display: none">
-                        Спасибо за Ваше сообщение. Оно успешно отправлено. Наш менеджер свяжется с Вами в ближайшее время.
-                    </div>
+<div class="parser-fos p-t_50 p-b_50">
+    <div class="container">
+        <div class="t-t_uppercase f-w_600 t-a_center f-z_23 m-b_10">Получите <span class="t-color_orange">дизайн-проект в подарок!*</span></div>
+        <div class="t-a_center m-b_10">Наш дизайнер с опытом работы более 10 лет в ИКЕА разработает дизайн-проект вашей кухни!</div>
+        <div id="{{ $widget->id }}" class="feedback" not-hide>
+            <div class="row">
+                <div class="col-md-6 col-lg-2">
+                    <label>
+                        <input name="name" type="text" required placeholder="{{ $widget->fields["name"] }}"/>
+                    </label>
                 </div>
-                <div class="f-z_12">*Услуга по разработке дизайн-проекта предоставляется бесплатно при 100% оплате кухни под заказ (базовая стоимость составляет 4 000 рублей). Подробности у менеджеров.</div>
+                <div class="col-md-6 col-lg-3">
+                    <label>
+                        <input name="phone" type="tel" required placeholder="{{ $widget->fields["phone"] }}"/>
+                    </label>
+                </div>
+                <div class="col-md-9 col-lg-5">
+                    <label>
+                        <textarea placeholder="{{ $widget->fields["question"] }}"></textarea>
+                    </label>
+                </div>
+                <div class="col-md-3 col-lg-2">
+                    <label><button class="btn-form" type="button">ОТПРАВИТЬ</button></label>
+                </div>
+                <div class="col-12">
+                    <label class="f-z_14">
+                        <input type="checkbox" name="agreement" value="{{ $widget->fields["agreement"] }}"> Я <a href="/page/soglasie-na-obrabotku-personalnyx-dannyx" target="_blank">согласен</a> на обработку персональных данных. Подробнее об этом в <a href="/page/politika-obrabotki-personalnyx-dannyx" target="_blank">политике конфиденциальности</a>
+                    </label>
+                </div>
             </div>
         </div>
-
+        <div id="{{ $widget->id }}-callback" class="form-send-message" style="display: none">
+            Спасибо за Ваше сообщение. Оно успешно отправлено. Наш менеджер свяжется с Вами в ближайшее время.
+        </div>
+    </div>
+</div>
 
 
