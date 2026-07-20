ab274d923b38968816226d8848cf8857e89b6a37
Saint <r.a.moiseenko@gmail.com>
2026-07-20 23:05:42 +0200

    ContentSection++

--- resources/views/shop/content/post.blade.php
+++ resources/views/shop/content/post.blade.php
@@ -11,12 +11,20 @@
 
 @section('content')
     @foreach($pageData->blocks as $block)
+        @if($block->section == "content")
         <div class="widget mt-4">
             @include('widgets::' . $block->widget->category . '.' . $block->widget->slug, ['params' => $block->widget->params])
         </div>
+        @endif
     @endforeach
 @endsection
 
 @section('bottom-content')
-
+    @foreach($pageData->blocks as $block)
+        @if($block->section == "bottom-content")
+        <div class="widget mt-4">
+            @include('widgets::' . $block->widget->category . '.' . $block->widget->slug, ['params' => $block->widget->params])
+        </div>
+        @endif
+    @endforeach
 @endsection
