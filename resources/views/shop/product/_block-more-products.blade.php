11fd4b423356aa09635c56675f6a4bd7c3fe316c
PC2MSIMAG <w@mail.ru>
2026-07-21 16:19:11 +0200

    fixed Livewire

---
+++ resources/views/shop/product/_block-more-products.blade.php
@@ -0,0 +1,29 @@
+<div class="block-more-product">
+    <div class="container"><div class="f-z_23 m-b_10 f-w_600 t-t_uppercase">НЕ СМОГЛИ НАЙТИ ТО, ЧТО ХОТЕЛИ?</div>
+        <div>
+            <p>Есть два решения:</p>
+            <ol>
+                <li class="m-b_10">Наши менеджеры помогут Вам с подбором. Для этого Вам просто надо связаться с нами по телефону: <a href="tel:+74012373730" class="t-color_orange">+7(4012) 37-37-30</a><br>либо в мессенджерах
+                    <a href="https://max.ru/u/f9LHodD0cOJAz9dknqnApj5TZ0VgcaJc5-w2ln7qMfzbHmc5Jtaw5fJtyls" target="_blank"><svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 80 80" fill="none">
+                            <rect width="80" height="80" rx="40" fill="url(#paint0_linear_5_55)"></rect>
+                            <rect width="80" height="80" rx="40" fill="url(#paint1_radial_5_55)"></rect>
+                            <path fill-rule="evenodd" clip-rule="evenodd" d="M40.4931 62.4927C35.9886 62.4927 33.8953 61.8322 30.2566 59.1904C27.955 62.1625 20.6668 64.4851 20.3489 60.5113C20.3489 57.5284 19.6913 55.0077 18.946 52.2558C18.0583 48.8654 17.05 45.0899 17.05 39.6192C17.05 26.5533 27.7249 16.7236 40.3726 16.7236C53.0312 16.7236 62.9499 27.0376 62.9499 39.7403C62.9924 52.2464 52.9451 62.426 40.4931 62.4927ZM40.6794 28.0173C34.52 27.6981 29.7196 31.98 28.6565 38.6946C27.7797 44.2534 29.336 51.0229 30.6621 51.3752C31.2978 51.5293 32.8979 50.2304 33.8953 49.2287C35.5444 50.373 37.4649 51.0602 39.4629 51.2211C45.8451 51.5294 51.2985 46.6495 51.727 40.2467C51.9764 33.8302 47.0625 28.3955 40.6794 28.0283V28.0173Z" fill="white"></path>
+                            <defs>
+                                <linearGradient id="paint0_linear_5_55" x1="9.42776" y1="60.8429" x2="80" y2="40" gradientUnits="userSpaceOnUse">
+                                    <stop stop-color="#44CCFF"></stop>
+                                    <stop offset="0.662" stop-color="#5533EE"></stop>
+                                    <stop offset="1" stop-color="#9933DD"></stop>
+                                </linearGradient>
+                                <radialGradient id="paint1_radial_5_55" cx="0" cy="0" r="1" gradientUnits="userSpaceOnUse" gradientTransform="translate(-1.90326 -35.1348) rotate(51.356) scale(97.0814 40)">
+                                    <stop stop-color="#0000FF"></stop>
+                                    <stop offset="1" stop-opacity="0"></stop>
+                                </radialGradient>
+                            </defs>
+                        </svg></a>
+                    <a href="https://t.me/NordiHomeBot" target="_blank"><svg role="presentation" width="25" height="25" viewBox="0 0 50 50" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M25 50C38.8071 50 50 38.8071 50 25C50 11.1929 38.8071 0 25 0C11.1929 0 0 11.1929 0 25C0 38.8071 11.1929 50 25 50Z" fill="#0087D0"></path><path d="M36.11 13.0399L9.40999 22.7999C8.86999 22.9999 8.85999 23.7999 9.38999 24.0299L16.23 26.7199L18.78 34.4099C18.93 34.8199 19.47 34.9599 19.81 34.6799L23.73 31.1899L31.17 35.9099C31.55 36.1499 32.06 35.9399 32.15 35.5099L36.99 13.7599C37.09 13.2799 36.59 12.8699 36.11 13.0599V13.0399ZM20.03 28.1599L19.6 32.1199L17.53 26.0299L32.1 16.8699L20.03 28.1699V28.1599Z" fill="white"></path></svg></a></li>
+                <li>Вы можете сделать заказ из полного ассортимента IKEA.pl, а мы доставим выбранные позиции в кратчайшие сроки, не зависимо от веса и объёма.</li>
+            </ol>
+        </div>
+        <a href="/calculate/" class="btn btn-white t-t_uppercase f-z_14 m-t_20">Заказать</a>
+    </div>
+</div>
