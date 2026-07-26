0c81ceadfa2c529b1e09e58716e6b927dab505a5
Saint <r.a.moiseenko@gmail.com>
2026-07-26 18:12:15 +0200

    fixed+

--- resources/views/shop/content/post.blade.php
+++ resources/views/shop/content/post.blade.php
@@ -38,4 +38,43 @@
             </div>
         @endif
     @endforeach
+
+
+    <div class="parser-fos p-t_50 p-b_50 m-t_20">
+        <div class="container">
+            <div class="t-t_uppercase f-z_35 t-a_center f-w_600">Остались вопросы?</div>
+            <div class="t-a_center m-t_10 m-b_20">Мы готовы ответить на Ваши вопросы: заполните форму ниже, и наш менеджер перезвонит Вам в ближайшее время.</div>
+            <div id="post" class="feedback-form" not-hide="">
+                <input type="hidden" name="form" value="Записи блога. Остались вопросы"/>
+                <div class="row">
+                    <div class="col-md-6 col-lg-2">
+                        <label>
+                            <input name="name" type="text" required="" placeholder="Имя и Фамилия">
+                        </label>
+                    </div>
+                    <div class="col-md-6 col-lg-3">
+                        <label>
+                            <input name="phone" type="tel" required="" placeholder="Ваш телефон: +79097589135">
+                        </label>
+                    </div>
+                    <div class="col-md-9 col-lg-5">
+                        <label>
+                            <textarea placeholder="Опишите Ваш вопрос или оставьте это поле пустым"></textarea>
+                        </label>
+                    </div>
+                    <div class="col-md-3 col-lg-2">
+                        <label><button class="btn-form" type="button">ОТПРАВИТЬ</button></label>
+                    </div>
+                    <div class="col-12">
+                        <label class="f-z_14">
+                            <input type="checkbox" name="agreement" value="Согласие на обработку персональных данных"> Я <a href="/page/soglasie-na-obrabotku-personalnyx-dannyx" target="_blank">согласен</a> на обработку персональных данных. Подробнее об этом в <a href="/page/politika-obrabotki-personalnyx-dannyx" target="_blank">политике конфиденциальности</a>
+                        </label>
+                    </div>
+                </div>
+            </div>
+            <div id="post-callback" class="form-send-message" style="display: none">
+                Спасибо за Ваше сообщение. Оно успешно отправлено. Наш менеджер свяжется с Вами в ближайшее время.
+            </div>
+        </div>
+    </div>
 @endsection
