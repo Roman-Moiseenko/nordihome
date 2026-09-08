36b3a521e5570566131b18723c4c6b60886288e1
Saint <r.a.moiseenko@gmail.com>
2026-09-08 16:48:15 +0200

    fixed

---
+++ app/Modules/Catalog/Database/Migrations/2026_09_08_150000_update_attributes_categories_foreign_keys_cascade.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('attributes_categories', function (Blueprint $table) {
+            $table->dropForeign(['attribute_id']);
+            $table->dropForeign(['category_id']);
+
+            $table->foreign('attribute_id')
+                ->references('id')
+                ->on('attributes')
+                ->onDelete('cascade');
+
+            $table->foreign('category_id')
+                ->references('id')
+                ->on('categories')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('attributes_categories', function (Blueprint $table) {
+            $table->dropForeign(['attribute_id']);
+            $table->dropForeign(['category_id']);
+
+            $table->foreign('attribute_id')
+                ->references('id')
+                ->on('attributes')
+                ->onDelete('restrict');
+
+            $table->foreign('category_id')
+                ->references('id')
+                ->on('categories')
+                ->onDelete('restrict');
+        });
+    }
+};
