8bd4c1517e4d2fe3384bdc09df4b1aa875a687e0
Saint <r.a.moiseenko@gmail.com>
2026-09-12 20:23:39 +0200

    fixed

---
+++ app/Modules/Cart/Domain/Interfaces/CartRepositoryInterface.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Modules\Cart\Domain\Interfaces;
+
+use App\Modules\Cart\Domain\Entities\CartItemEntity;
+use App\Modules\Shop\Application\DTOs\ClientContext;
+
+interface CartRepositoryInterface
+{
+    public function save(CartItemEntity $entity, ClientContext $client): CartItemEntity;
+    public function clearCart(ClientContext $client): void;
+
+    /**
+     * @param ClientContext $client
+     * @return CartItemEntity[]
+     */
+    public function getAll(ClientContext $client): array;
+
+    public function getItemByProductId(int $productId, ClientContext $client):? CartItemEntity;
+
+    public function getItemById(int $id, ClientContext $client): CartItemEntity;
+
+    public function removeByProductId(int $productId, ClientContext $client): void;
+
+
+    /**
+     * Вспомогательные методы для корзины Куки
+     * Используются при переносе корзины из куки в хранилище
+     */
+
+    /**
+     * @param ClientContext $client
+     * @return CartItemEntity[]
+     */
+    public function getItemsCookie(ClientContext $client): array;
+
+    public function clearCookie(ClientContext $client): void;
+
+
+}
